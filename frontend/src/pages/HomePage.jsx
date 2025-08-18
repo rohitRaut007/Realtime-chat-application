16a004b51af33bf3347f7320e06aeb1b92b1890f
rohitRaut007 <rohit.raut2612@gmail.com>
2025-08-19 03:42:32 +0530

    deploy

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import { useChatStore } from "../store/useChatStore";
-import Sidebar from "../components/sidebar";
+import Sidebar from "../components/Sidebar";
 import NoChatSelected from "../components/NoChatSelected";
 import ChatContainer from "../components/ChatContainer";
 
